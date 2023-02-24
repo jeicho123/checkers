98ea449ad1373268f4627a506db17a2174ccf1c7
Patrick Lin <patricklin@Patricks-MacBook-Pro.local>
2023-02-23 20:44:03 -0600

    Updated test_cases

--- src/test_checkers.py
+++ src/test_checkers.py
@@ -22,54 +22,54 @@ def test_player_valid_moves():
     g._remove((1, 2))
     g._remove((1, 4))
     
-    assert g.player_valid_moves(PieceColor.BLACK) == {}
+    assert g.player_valid_moves("BLACK") == {}
 
 def test_block_win():
     g = Game(2)
-    g.move(PieceColor.RED, (4, 1), (3, 0))
-    g.move(PieceColor.RED, (3, 0), (2, 1))
-    g.move(PieceColor.RED, (4, 3), (3, 2))
-    g.move(PieceColor.RED, (3, 2), (2, 3))
-    g.move(PieceColor.RED, (4, 5), (3, 4))
-    g.move(PieceColor.RED, (3, 4), (2, 5))
-    g.move(PieceColor.RED, (5, 0), (4, 1))
-    g.move(PieceColor.RED, (4, 1), (3, 0))
-    g.move(PieceColor.RED, (5, 2), (4, 3))
-    g.move(PieceColor.RED, (4, 3), (3, 2))
-    g.move(PieceColor.RED, (5, 4), (4, 5))
-    g.move(PieceColor.RED, (4, 5), (3, 4))
+    g.move("RED", (4, 1), (3, 0))
+    g.move("RED", (3, 0), (2, 1))
+    g.move("RED", (4, 3), (3, 2))
+    g.move("RED", (3, 2), (2, 3))
+    g.move("RED", (4, 5), (3, 4))
+    g.move("RED", (3, 4), (2, 5))
+    g.move("RED", (5, 0), (4, 1))
+    g.move("RED", (4, 1), (3, 0))
+    g.move("RED", (5, 2), (4, 3))
+    g.move("RED", (4, 3), (3, 2))
+    g.move("RED", (5, 4), (4, 5))
+    g.move("RED", (4, 5), (3, 4))
 
-    g.end_turn(PieceColor.RED, "End Turn")
-    assert g.get_winner() == PieceColor.RED
+    g.end_turn("RED", "End Turn")
+    assert g.get_winner() == "RED"
 
 def test_move():
     g = Game(5)
-    g.move(PieceColor.BLACK, (4, 1), (5, 0))
+    g.move("BLACK", (4, 1), (5, 0))
 
     assert g._get((5, 0)) is not None
 
 def test_single_jump():
     g = Game(5)
-    g.move(PieceColor.BLACK, (4, 1), (5, 0))
-    g.move(PieceColor.RED, (7, 2), (6, 1))
-    g.move(PieceColor.BLACK, (5, 0), (7, 2))
+    g.move("BLACK", (4, 1), (5, 0))
+    g.move("RED", (7, 2), (6, 1))
+    g.move("BLACK", (5, 0), (7, 2))
 
     assert g._get((7, 2)) is not None
 
 def test_multijump():
     g = Game(5)
-    g.move(PieceColor.BLACK, (4, 1), (5, 0))
-    g.move(PieceColor.RED, (7, 2), (6, 1))
+    g.move("BLACK", (4, 1), (5, 0))
+    g.move("RED", (7, 2), (6, 1))
     g._remove((9, 0))
-    g.move(PieceColor.BLACK, (5, 0), (9, 0))
+    g.move("BLACK", (5, 0), (9, 0))
 
     assert g._get((9, 0)) is not None
 
 def test_promote():
     g = Game(2)
-    g.move(PieceColor.BLACK, (1, 0), (2, 1))
-    g.move(PieceColor.BLACK, (2, 1), (3, 2))
+    g.move("BLACK", (1, 0), (2, 1))
+    g.move("BLACK", (2, 1), (3, 2))
     g._remove((0, 3))
-    g.move(PieceColor.RED, (4, 3), (0, 3))
+    g.move("RED", (4, 3), (0, 3))
 
     assert g._get((0, 3)).is_king()
