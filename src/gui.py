ae50d683e171c691b15f15bdd62967d41a20025c
Tiffany Jaewon Lee <tiffanyjlee@linux6.cs.uchicago.edu>
2023-02-24 00:57:44 -0600

    updated gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,15 +1,17 @@
 """
 GUI for Checkers!
 """
+#took inspiration from connectm! 
+
 import os
 import sys
 from typing import Union, Dict
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
-# import click
+import click
 
-from checkers import Game, Piece
+from checkers import Game, PieceColor
 from bot import randomBot, smartBot
 
 WIDTH = 800
@@ -28,7 +30,7 @@ class GUIPlayer:
     name: str
     bot: Union[None, randomBot, smartBot]
     board: Game
-    color: str
+    color: PieceColor
 
     def __init__(self, n, player, board, color, opponent_color):
         """
@@ -98,36 +100,76 @@ def create_board(surface: pygame.surface.Surface, board):
             center, radius)
     
 
-def play_checkers(board, players, bot_delay):
+def play_checkers(board, surface, players):
     """
     Plays a game of Checkers in Pygame.
 
     Args:
         board: The checkers board
         players (dict): A dictionary mapping piece colors to
-            TUIPlayer objects
-        bot_delay: The bot has a delay in which it waits a few
-            seconds before making a move.
+            GUIPlayer objects
 
     Returns: None
     """
     pygame.init()
     pygame.display.set_caption("Checkers")
-    surface = pygame.display.set_mode(WIDTH, HEIGHT)
-    clock = pygame.time.Clock()
 
     #starting player is a black piece
-    current_player = players["BLACK"]
+    current_player = PieceColor.BLACK
+
+    size = len(board.board_to_str)
 
     while board.get_winner() is None:
         events = pygame.event.get()
-        column = None
+
+        start_row = None
+        start_col = None
+        end_row = None
+        end_col = None
+
         for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
             if current_player.bot is None and event.type == pygame.KEYUP:
-                key = event.unicode
-                if key in "1234567":
-                    v = int(key) - 1
+                num = event.unicode
+                if num in range(size):
+                    if start_row is None:
+                        start_row = num
+                    elif start_col is None:
+                        start_col = num
+
+                        start_coord = start_row, start_col
+                        if start_coord in board.player_valid_moves(current_player):
+                            for x in board.piece_all_valid(current_player):
+                                for y in x:
+                                    for position in y:
+                                        pygame.draw.rect(surface, WHITE,
+                                                         rect=position, width=2)
+                        else:
+                            start_row = None
+                            start_col = None
+                    elif end_row is None:
+                        end_row = num
+                    elif end_col is None:
+                        end_col = num
+                        end_coord = end_row, end_col
+                        if board.valid_move(start_coord, end_coord):
+                            board.move(current_player, start_coord, end_coord)
+                            start_row = None
+                            start_col = None
+                            end_row = None
+                            end_col = None
+                            if not board.turn_incomplete():
+                                if current_player == PieceColor.RED:
+                                    current_player == PieceColor.BLACK
+                                elif current_player == PieceColor.BLACK:
+                                    current_player == PieceColor.RED
+                        else:
+                            end_row = None
+                            end_col = None
+
+    winner = board.get_winner()
+    if winner is not None:
+        print(f"The winner is {players[winner].name}!")
