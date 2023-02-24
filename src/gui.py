f25fa6e9c58f1bdb9150b587e29537b34e4b6b6e
Tiffany Jaewon Lee <tiffanyjlee@linux6.cs.uchicago.edu>
2023-02-24 01:02:01 -0600

    updated gui.py

--- src/gui.py
+++ src/gui.py
@@ -53,15 +53,14 @@ class GUIPlayer:
         self.board = board
         self.color = color
 
-    def create_board(surface: pygame.surface.Surface, board):
-        """
-        Creates and draws the board in its current state.
+def create_board(surface: pygame.surface.Surface, board):
+    """
+    Creates and draws the board in its current state.
 
-        Args:
-            surface: Pygame surface that the board is drawn on
-            board: The board that needs to be drawn
+    Args:
+        surface: Pygame surface that the board is drawn on
+        board: The board that needs to be drawn
 
-<<<<<<< HEAD
     Returns: None
     """
     board_grid = board.board_to_str()
@@ -99,7 +98,6 @@ class GUIPlayer:
             radius = rh // 2 - 8
             pygame.draw.circle(surface, color,
             center, radius)
-    
 
 def play_checkers(board, surface, players):
     """
@@ -174,76 +172,3 @@ def play_checkers(board, surface, players):
     winner = board.get_winner()
     if winner is not None:
         print(f"The winner is {players[winner].name}!")
-=======
-        Returns: None
-        """
-        board_grid = board.board_to_str()
-        rows = len(board_grid)
-        cols = len(board_grid[0])
-
-        # Compute row height and column width
-        rh = HEIGHT // rows
-        cw = WIDTH // cols
-
-        # Fill entire surface window with red
-        surface.fill(DARK_BROWN)
-
-        # Drawing each square of the board
-        for row in range(rows):
-            for col in range(row % 2, rows, 2):
-                rect = (col * cw, row * rh, cw, rh)
-                pygame.draw.rect(surface, LIGHT_BROWN, rect=rect)
-
-        # Drawing each piece
-        for i, r in enumerate(board_grid):
-            for j, piececolor in enumerate(r):
-                if piececolor == "R":
-                    color = (139,0,0)
-                elif piececolor == "r":
-                    color = RED
-                elif piececolor == "B":
-                    color = (128,128,128)
-                elif piececolor == "b":
-                    color = BLACK
-                else:
-                    continue
-
-                center = ((j * cw) + (cw // 2), (i * rh) + (rh // 2))
-                radius = rh // 2 - 8
-                pygame.draw.circle(surface, color,
-                center, radius)
-
-    def play_checkers(board, players, bot_delay):
-        """
-        Plays a game of Checkers in Pygame.
-
-        Args:
-            board: The checkers board
-            players (dict): A dictionary mapping piece colors to
-                TUIPlayer objects
-            bot_delay: The bot has a delay in which it waits a few
-                seconds before making a move.
-
-        Returns: None
-        """
-        pygame.init()
-        pygame.display.set_caption("Checkers")
-        surface = pygame.display.set_mode(WIDTH, HEIGHT)
-        clock = pygame.time.Clock()
-
-        #starting player is a black piece
-        current_player = players["BLACK"]
-
-        while board.get_winner() is None:
-            events = pygame.event.get()
-            column = None
-            for event in events:
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-                if current_player.bot is None and event.type == pygame.KEYUP:
-                    key = event.unicode
-                    if key in "1234567":
-                        v = int(key) - 1
->>>>>>> 339464f306a766345c5b20260945f99cf73c5211
