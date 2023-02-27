8f957c1202bbe395f6d7f3e2edb07907c5eb529c
Patrick Lin <patricklin@Patricks-MacBook-Pro.local>
2023-02-26 18:56:02 -0600

    Started on copying methods over

--- src/checkers_2.py
+++ src/checkers_2.py
@@ -19,18 +19,43 @@ class Piece:
         self._king = True
 
 class Board:
-    def __init(self, n):
-        self._board = [[None] * n for _ in range(n)]
+    def __init__(self, n):
+        self._grid = [[None] * n for _ in range(n)]
 
     def get(coord):
         row, col = coord
-        return self._board[row][col]
+        return self._grid[row][col]
 
-    def set(coord, piece):
+    def set(coord, item):
         row, col = coord
-        self._board[row][col] = piece
+        self._grid[row][col] = item
+    
+    def remove(coord):
+        self.set(coord, None)
 
     def move(start, end):
         piece = self.get(start)
         self.set(start, None)
         self.set(end, piece)
+
+    def board_to_str(self):
+        grid = []
+        for row in self._grid:
+            new_row = []
+            for square in row:
+                if square is None:
+                    new_row.append(" ")
+                elif square.get_color() == PieceColor.BLACK and square.is_king():
+                    new_row.append("B")
+                elif square.get_color() == PieceColor.BLACK and not square.is_king():
+                    new_row.append("b")
+                elif square.get_color() == PieceColor.RED and square.is_king():
+                    new_row.append("R")
+                else:
+                    new_row.append("r")
+            grid.append(new_row)
+        return grid
+
+class CheckersGame:
+    def __init__(self, nrows):
+        self._board = Board(2 * nrows + 2)
