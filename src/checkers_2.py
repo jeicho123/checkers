9a52b36cc813555576db01e85f15b5eecb1e36de
Patrick Lin <patricklin@Patricks-MacBook-Pro.local>
2023-02-26 19:52:53 -0600

    Finished reset board method

--- src/checkers_2.py
+++ src/checkers_2.py
@@ -22,18 +22,18 @@ class Board:
     def __init__(self, n):
         self._grid = [[None] * n for _ in range(n)]
 
-    def get(coord):
+    def get(self, coord):
         row, col = coord
         return self._grid[row][col]
 
-    def set(coord, item):
+    def set(self, coord, item):
         row, col = coord
         self._grid[row][col] = item
     
-    def remove(coord):
+    def remove(self, coord):
         self.set(coord, None)
 
-    def move(start, end):
+    def move(self, start, end):
         piece = self.get(start)
         self.set(start, None)
         self.set(end, piece)
@@ -58,4 +58,42 @@ class Board:
 
 class CheckersGame:
     def __init__(self, nrows):
+        self._board = []
+        self._black_pieces = []
+        self._red_pieces = []
+        self._jumping = None
+        self._winner = None
+        self._draw_offered = False
+        self._reset(nrows)
+
+    def __str__(self):
+        board_string = ""
+        for row in self.board_to_str():
+            for sqaure in row:
+                board_string += sqaure
+            board_string += "\n"
+        return board_string
+
+    def board_to_str(self):
+        return self._board.board_to_str()
+
+    def _reset(self, nrows):
         self._board = Board(2 * nrows + 2)
+        self._red_pieces = []
+        self._black_pieces = []
+        self._jumping = None
+        self._winner = None
+        self.draw_offered = False
+
+        size = 2 * nrows + 2
+        # steup game board
+        for r in range(size):
+            for c in range(size):
+                if r < nrows and r % 2 != c % 2:
+                    self._board.set((r, c), Piece(PieceColor.BLACK))
+                    self._black_pieces.append((r, c))
+                elif r >= size - nrows and r % 2 != c % 2:
+                    self._board.set((r, c), Piece(PieceColor.RED))
+                    self._red_pieces.append((r, c))
+                else:
+                    self._board.set((r, c), None)
